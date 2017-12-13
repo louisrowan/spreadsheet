e9cdb26dcdfacd9c7a8cd69b67f643418faa1901
Louis Rowan - Vendor <LRowan@WalmartLabs.com>
2017-12-12 20:59:33 -0800

    adds basic style

--- index.js
+++ index.js
@@ -2,19 +2,35 @@
 
 const main = document.getElementById('main')
 
+const ROW_COUNT = 4;
+const COL_COUNT = 4;
+
+main.style.width = `${100 * COL_COUNT}px`;
 
 
 function cell () {
 
     this.div = document.createElement('div');
+    cellStyle(this.div);
 
 
     this.input = document.createElement('input');
+    inputStyle(this.input)
 
     this.input.addEventListener('input', (e) => {
 
 
         this.input = e.target.value;
+    })
+
+    this.input.addEventListener('focus',(e) => {
+
+        this.input.style['boxShadow'] = '0px 0px 2px 2px black inset';
+    })
+
+    this.input.addEventListener('blur',(e) => {
+
+        this.input.style['boxShadow'] = 'none';
     })
 
 
@@ -35,9 +51,29 @@ cell.prototype.setText = function(val) {
     return this.input.value
 }
 
-const t = new cell()
-main.appendChild(t.div)
 
-t.setText('hello')
-t.getText()
+for (let i = 0; i < ROW_COUNT; ++i) {
 
+
+    for (let j = 0; j < COL_COUNT; ++j) {
+
+        let _cell = new cell()
+
+        main.appendChild(_cell.div)
+    }
+}
+
+function cellStyle(div) {
+    
+    div.style.width = '100px';
+    div.style.height = '50px';
+    div.style.display = 'inline-block';
+}
+
+function inputStyle(input) {
+    
+    input.style.width = '100%';
+    input.style.height = '100%';
+    input.style.outline = 'none';
+    input.style
+}

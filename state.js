1e90413ec5b6f07510b0781b095a1949ce4caedd
Louis Rowan - Vendor <louis.rowan@icloud.com>
2017-12-18 22:21:34 -0800

    add some notes and explanation of state elements

--- state.js
+++ state.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const _state = {
-    allCells: [],
-    activeCells: [],
-    mousedown: false,
-    draggableDiv: {},
-    startCellRect: {},
-    endCellRect: {},
-    copy: {}
+    allCells: [], // array of cell objects containing all cells in spreadsheet
+    activeCells: [], // array of cell objects containing 'active' cells
+    mousedown: false, // boolean to determine when mouseover is a drag event
+    draggableDiv: {}, // htmlElement that is used to show multi-selection and drag events
+    startCellRect: {}, // cell at start of draggableDiv
+    endCellRect: {}, // cell at end of draggableDiv
+    copy: {}, // array of cell objects containing cells on cut/copy clipboard
+    commandActive: false // boolean to determine if command key is being held down
 };
--- window.js
+++ window.js
@@ -11,29 +11,57 @@ window.addEventListener('keydown', (e) => {
         e.key === 'ArrowRight' ||
         e.key === 'ArrowUp' ||
         e.key === 'ArrowDown') {
+        handleMove(e);
+    }
 
-        const activeElement = _state.allCells.find((cell) => 'cell-' + cell.id === document.activeElement.id);
-        if (!activeElement) { return };
-        let row = activeElement.row;
-        let column = activeElement.column;
+    if (e.key === 'Meta') {
+        _state.commandActive = true;
+    }
 
-        switch (e.key) {
-            case "ArrowLeft":
-                column = column > 0 ? --column : 0
-                break;
-            case "ArrowRight":
-                column = column < COL_COUNT - 1 ? ++column : 0
-                break;
-            case "ArrowUp":
-                row = row > 0 ? --row : 0
-                break;
-            case "ArrowDown":
-                row = row < ROW_COUNT - 1 ? ++row : 0
-                break;
+    if (_state.commandActive) {
+        if (e.key === 'c') {
+            console.log('copy');
         }
-
-        const newElement = _state.allCells.find((cell) => cell.row === row && cell.column === column);
-        newSelectedCell(newElement);
-        newElement.input.focus();
+        else if (e.key === 'x') {
+            console.log('cut');
+        }
+        else if (e.key === 'p') {
+            e.preventDefault();
+            console.log('pase');
+        }
     }
 })
+
+window.addEventListener('keyup', (e) => {
+
+    if (e.key === 'Meta') {
+        _state.commandActive = false;
+    }
+})
+
+function handleMove(e) {
+
+    const activeElement = _state.allCells.find((cell) => 'cell-' + cell.id === document.activeElement.id);
+    if (!activeElement) { return };
+    let row = activeElement.row;
+    let column = activeElement.column;
+
+    switch (e.key) {
+        case "ArrowLeft":
+            column = column > 0 ? --column : 0
+            break;
+        case "ArrowRight":
+            column = column < COL_COUNT - 1 ? ++column : 0
+            break;
+        case "ArrowUp":
+            row = row > 0 ? --row : 0
+            break;
+        case "ArrowDown":
+            row = row < ROW_COUNT - 1 ? ++row : 0
+            break;
+    }
+
+    const newElement = _state.allCells.find((cell) => cell.row === row && cell.column === column);
+    newSelectedCell(newElement);
+    newElement.input.focus();
+}
